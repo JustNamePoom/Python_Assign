b54b8af1753e37066bd542786aa5f9a83555937e
JustNamePoom <tummanoon.btw@gmail.com>
2026-03-14 07:59:28 -0700

    Create functionHomework.py

---
+++ functionHomework.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplieNumber(x,y):
+    print(x*y)
+def devideNumber(x,y):
+    print(x/y)
+addNumber(10,5)
+minusNumber(10,5)
+multiplieNumber(10,5)
+devideNumber(10,5)
